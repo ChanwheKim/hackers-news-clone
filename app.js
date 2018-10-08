72fdc3c7b5e0a68e3fdda2b4fcbedb4b5f12cbd3
ChanwheKim <chanwhe.kim@gmail.com>
2018-10-08 09:30:19 +0900

    feat: Add a function for separate XMLHttpRequest for details of each article

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 (function() {
 
-    topStoriesRequest();
+    topStoriesRequest(5);
 
-    function topStoriesRequest() {
+    function topStoriesRequest(num) {
 
-        var url = 'https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/topstories.json'
+        let url = 'https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/topstories.json'
         
-        var req = new XMLHttpRequest();
+        let req = new XMLHttpRequest();
         req.onreadystatechange = function() {
             if(req.readyState === 4 && req.status === 200) {
+                let IDs = JSON.parse(req.responseText);
+                let startIdx = 0;
 
-                console.log(JSON.parse(req.responseText));
+                requestArticles(startIdx, num);
+                
+                function requestArticles(start, add) {
+                    let endIdx = start + add;
 
+                    for(var i = start; i < endIdx; i++) {
+
+                        let reqArticle = new XMLHttpRequest();
+                        reqArticle.onreadystatechange = function() {
+                            if(reqArticle.readyState === 4 && reqArticle.status === 200) {
+                                console.log(reqArticle.responseText);
+                            }
+                        };
+    
+                        reqArticle.onerror = function(err) {
+                            console.log('Error ', err);
+                        };
+    
+                        reqArticle.open('GET', `https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/item/${IDs[i]}.json?print=pretty`);
+                        reqArticle.send();
+
+                    }
+                };
             }
         };
 
