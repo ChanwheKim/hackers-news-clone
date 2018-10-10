add85c3cd9724276af0dbde68a3052ea10dd77b1
ChanwheKim <chanwhe.kim@gmail.com>
2018-10-10 23:52:09 +0900

    feat: Add HTTP requests for separate articles
    I saved separate article objects into data object.

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+
+// Tempo
+const data = {};
+
 (function() {
+
 
     //// Http request for top stories list
     let topStoriesUrl = 'https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -6,9 +11,32 @@
     fetch(topStoriesUrl)
     .then(handleError)
     .then(parseJSON)
-    .then(requestArticles)
+    .then(saveArticleIDs)
+    .then(requestArticle)
     .catch(displayError)
 
+    function requestArticle(list, startIdx = 0, endIdx = 5) {
+        
+        for(var i = startIdx; i < endIdx; i++) {
+            let articleUrl = `
+                https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/item/${data.IDs[i]}.json?print=pretty
+                `;
+
+            // For the reference in the future
+            if(!data.articles[data.IDs[i]]) {
+                data.articles[data.IDs[i]] = null;
+            }
+
+            fetch(articleUrl)
+            .then(handleError)
+            .then(parseJSON)
+            .then(saveArticles)
+
+        }
+
+    };
+
+    //// reusable functions
     function handleError(response) {
         if(!response.ok) {
             throw Error(response.status);;
@@ -20,14 +48,23 @@
         return response.json();
     };
 
-    function requestArticles(data) {
-        console.log(data);
-        
-    };
-
     function displayError(error) {
         console.log(error + ' from displayError function.');
     };
+
+    //// request for top-stories
+    function saveArticleIDs(ids) {
+        data.IDs = ids;
+        data.articles = {};
+        data.htmlStr = {};
+        return data.IDs;
+    };
+
+    //// request for separate articles
+    function saveArticles(article) {
+        data.articles[article.id] = article;
+    }
+
 
 })();
     
